ae9972be5076a37d1206e6cd310c55b106f246b6
Becky Rush <becky.rush14@yahoo.co.uk>
2017-10-17 18:46:20 +0100

    start to play with movement for game

--- js/main.js
+++ js/main.js
@@ -11,13 +11,42 @@ function init() {
 }
 
 function preload() {
-  console.log('preload');
+  game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 function create() {
-  console.log('create');
+  cursors = game.input.keyboard.createCursorKeys();
+
+  player = game.add.sprite(game.world.width/2 - 16, game.world.height - 50, 'dude');
+  game.physics.arcade.enable(player);
+  player.body.bounce.y = 0.2;
+  player.body.gravity.y = 300;
+  player.body.collideWorldBounds = true;
 }
 
 function update() {
-  console.log('update');
+  movePlayer();
+}
+
+function movePlayer() {
+    //  Reset the players velocity (movement)
+    player.body.velocity.x = 0;
+    player.frame = 4;
+
+    if (cursors.left.isDown && !cursors.right.isDown) {
+        player.body.velocity.x = -45;
+        player.body.velocity.y = -50;
+    }
+    else if (cursors.right.isDown && !cursors.left.isDown) {
+      player.body.velocity.x = 45;
+      player.body.velocity.y = -50;
+    }
+    else if (cursors.right.isDown && cursors.left.isDown) {
+      player.body.velocity.x = 0;
+      player.body.velocity.y = -70;
+    }
+    else {
+        player.body.velocity.y = 0;
+        player.body.gravity.y = 500;
+    }
 }
