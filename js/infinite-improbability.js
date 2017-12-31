d456b942bc8329dfd8351a7cdb5a0261c07c3307
Becky Rush <becky.rush14@yahoo.co.uk>
2017-12-31 11:59:51 +0000

    drop whale and petunias from iid

--- js/infinite-improbability.js
+++ js/infinite-improbability.js
@@ -1,4 +1,5 @@
 var currentScenario = 'reset';
+
 class ImprobabilityDrive {
   constructor() {
     let improbabilityDrive = DontPanic.game.add.group();
@@ -18,57 +19,65 @@ class ImprobabilityDrive {
     if (improbabilityDrive.generated == true) {
       improbabilityDrive.generated = false;
     } else {
-      console.log('improbabilityDrive generating!');
-      console.log(DontPanic.game.world.height);
       var button_iid = this.improbabilityDrive.create(DontPanic.game.world.centerX/5, 1050, 'red_button');
       button_iid.scale.x = 0.3;
       button_iid.scale.y = 0.3;
       button_iid.enableBody = true;
-      button_iid.animation = button_iid.animations.add('flash', [0,1,0,1,0,1], true);
+      button_iid.animation = button_iid.animations.add('flash', [0,1,0,1,0,1,0], true);
       button_iid.play('flash');
       button_iid.inputEnabled = true;
-      button_iid.events.onInputDown.add(this.regenerateAssets, this);
+      button_iid.events.onInputDown.add(this.triggerEvent, this);
       DontPanic.game.debug.body(button_iid);
-      console.log(button_iid);
       improbabilityDrive.generated = true;
     }
   }
 
   //could call either regenerate assets or trigger another event (whale?)
+  triggerEvent(button_iid){
+    var eventTriggered = Math.random() <= 0.5 ? this.regenerateAssets : this.randomEvent;
+    console.log(this);
+    let that = this;
+    eventTriggered(button_iid, that);
+  }
 
-  regenerateAssets(button_iid) {
+  regenerateAssets(button_iid, that) {
     console.log('iid triggered');
     var improbabilityDriveDuration = config[config.currentLevel]['infiniteImprobabilityDuration'];
-    this.improbabilityDurationTimer = DontPanic.game.time.events.loop(Phaser.Timer.SECOND * improbabilityDriveDuration, this.improbabilityDriveReset, this);
-    this.improbabilityDurationTimer.timer.start();
+    that.improbabilityDurationTimer = DontPanic.game.time.events.loop(Phaser.Timer.SECOND * improbabilityDriveDuration, that.assetReset, this);
+    that.improbabilityDurationTimer.timer.start();
     button_iid.kill();
     improbabilityDriveTriggered = true;
     currentScenario = 1;
-    background.background.loadTexture(improbabilityScenarios[1].background);
-    player.playerSprite.loadTexture(improbabilityScenarios[1].player, 0);
+    background.background.loadTexture(improbabilityScenarioAssets[1].background);
+    player.playerSprite.loadTexture(improbabilityScenarioAssets[1].player, 0);
     enemy.enemies.forEachExists((enemy) =>  {
-      enemy.loadTexture(improbabilityScenarios[1].enemy, 0);
+      enemy.loadTexture(improbabilityScenarioAssets[1].enemy, 0);
     });
     coins.coins.forEachExists((coin) =>  {
-      coin.loadTexture(improbabilityScenarios[1].coins);
+      coin.loadTexture(improbabilityScenarioAssets[1].coins);
     });
   }
 
-  improbabilityDriveReset() {
+  assetReset() {
     console.log('reset');
     currentScenario = 'reset';
-    background.background.loadTexture(improbabilityScenarios['reset'].background);
-    player.playerSprite.loadTexture(improbabilityScenarios['reset'].player, 0);
+    background.background.loadTexture(improbabilityScenarioAssets['reset'].background);
+    player.playerSprite.loadTexture(improbabilityScenarioAssets['reset'].player, 0);
     enemy.enemies.forEachExists((enemy) =>  {
-      enemy.loadTexture(improbabilityScenarios['reset'].enemy, 0);
+      enemy.loadTexture(improbabilityScenarioAssets['reset'].enemy, 0);
     });
     coins.coins.forEachExists((coin) =>  {
-      coin.loadTexture(improbabilityScenarios['reset'].coins);
+      coin.loadTexture(improbabilityScenarioAssets['reset'].coins);
     });
+  }
+
+  randomEvent() {
+    var eventTriggered = Math.random() <= 0.5 ? Whale : Petunias;
+    new eventTriggered();
   }
 }
 
-const improbabilityScenarios = {
+const improbabilityScenarioAssets = {
   reset: {
     player: `rocket_${config.playerColour}`, //get the right one from config
     enemy: 'enemyShip',
@@ -80,5 +89,31 @@ const improbabilityScenarios = {
     enemy: 'IID_enemy1',
     background: 'IID_background1',
     coins: 'IID_teacup',
+  }
+};
+
+class Whale {
+  constructor() {
+    var randomX = Math.floor(Math.random() * 300) + 100;
+    const whale = DontPanic.game.add.sprite(randomX, -100, 'whale');
+    whale.scale.x = 0.3;
+    whale.scale.y = 0.3;
+    DontPanic.game.physics.arcade.enable(whale);
+    whale.body.collideWorldBounds = false;
+    whale.body.gravity.y = 50;
+    this.whaleSprite = whale;
+  }
+}
+
+class Petunias {
+  constructor() {
+    var randomX = Math.floor(Math.random() * 320) + 25;
+    const petunias = DontPanic.game.add.sprite(randomX, -50, 'petunias');
+    petunias.scale.x = 0.1;
+    petunias.scale.y = 0.1;
+    DontPanic.game.physics.arcade.enable(petunias);
+    petunias.body.collideWorldBounds = false;
+    petunias.body.gravity.y = 60;
+    this.petuniasSprite = petunias;
   }
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let DontPanic = {};
 let player;
 let whale;
+let petunias;
 let enemy;
 let fuelbar;
 let background;
@@ -31,7 +32,6 @@ function preload() {
   DontPanic.game.load.image('playAgainButton', 'assets/button_play-again.png'); // placeholders for now
   DontPanic.game.load.spritesheet('rocket_red', 'assets/rocket_spritesheet1.png', 115, 175);
   DontPanic.game.load.spritesheet('rocket_blue', 'assets/rocket_spritesheet_blue1.png', 115, 175);
-  DontPanic.game.load.image('whale', 'assets/whale.png');
   DontPanic.game.load.spritesheet('enemyShip', 'assets/enemy_ship_spritesheet1.png', 440, 1140);
   DontPanic.game.load.spritesheet('red_button', 'assets/iid-button.png', 472, 472);
   DontPanic.game.load.image('heart', 'assets/life.png');
@@ -42,6 +42,8 @@ function preload() {
   DontPanic.game.load.spritesheet('IID_enemy1', 'assets/iid/iid_enemy1.png', 440, 1140);
   DontPanic.game.load.spritesheet('IID_player1', 'assets/iid/iid_player1.png', 115, 175);
   DontPanic.game.load.image('IID_teacup', 'assets/iid/tea.png');
+  DontPanic.game.load.image('whale', 'assets/iid/whale.png');
+  DontPanic.game.load.image('petunias', 'assets/iid/flowers.png');
 }
 
 function create() {
@@ -67,7 +69,6 @@ function startGame() {
   gameStarted = true;
   background = new Background();
   player = new Player();
-  whale = new Whale();
   enemy = new Enemy();
   coins = new Coins();
   lives = new Lives();
@@ -78,9 +79,14 @@ function startGame() {
 }
 
 function handleCollision() {
-  DontPanic.game.physics.arcade.overlap(player.playerSprite, whale.whaleSprite, obstacleCollision, null, this);
   DontPanic.game.physics.arcade.overlap(player.playerSprite, coins.coins, collectCoin, null, this);
   DontPanic.game.physics.arcade.overlap(player.playerSprite, enemy.enemies, abductPlayer, null, this);
+  if (whale) {
+    DontPanic.game.physics.arcade.overlap(player.playerSprite, whale.whaleSprite, obstacleCollision, null, this);
+  }
+  if (petunias) {
+    DontPanic.game.physics.arcade.overlap(player.playerSprite, petunias.petuniasSprite, obstacleCollision, null, this);
+  }
 }
 
 function obstacleCollision() {
