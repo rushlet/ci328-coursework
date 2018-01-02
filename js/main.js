47838a46f47a5ea97c7bc4d83be5426cf49b7926
Becky Rush <becky.rush14@yahoo.co.uk>
2018-01-02 14:52:35 +0000

    fix #34

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ let filter;
 let improbabilityDrive;
 let improbabilityDriveTriggered = false;
 let extraLife;
+let newBestScore = false;
 
 function init() {
   const gameWidth = 360;
@@ -116,18 +117,26 @@ function removeAllEntities() {
   coins.coins.kill();
   obstacle.obstacles.kill();
   improbabilityDrive.improbabilityDrive.kill();
+  extraLife.extraLives.kill();
 }
 
 function gameOver() {
+  distanceScore.distanceTimer.timer.stop();
+  checkBestDistance();
   DontPanic.game.gameOver = DontPanic.game.add.text(DontPanic.game.world.centerX, DontPanic.game.world.centerY * 0.75, 'GAME OVER', { font: '40px whoopass', fill: '#fff' });
   DontPanic.game.gameOver.anchor.setTo(0.5);
   DontPanic.game.camera.fade(0x0000000, 3000);
   DontPanic.game.camera.onFadeComplete.add(playAgainMenu, this);
   background.background.stopScroll();
-  gameStarted = false;
   removeAllEntities();
   stopGameAssetGeneration();
+  gameStarted = false;
+}
+
+function checkBestDistance() {
   if (currentDistance > parseInt(bestDistance)) {
     localStorage['bestDistance'] = currentDistance.toString();
+    bestDistance = localStorage['bestDistance'];
+    newBestScore = true;
   }
 }
